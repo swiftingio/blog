228cab13bf06f0885cd2f1691362c2d567b7eafe
Woronin, Bartlomiej {FPSA~Poznan} <bartlomiej.woronin@roche.com>
2016-02-06 23:32:33 +0100

    #4 Migration From Parse. Added Express app.

---
+++ #4-Migration-From-Parse/ExpressApp/index.js
@@ -0,0 +1,34 @@
+// Example express application adding the parse-server module to expose Parse
+// compatible API routes.
+
+var express = require('express');
+var ParseServer = require('parse-server').ParseServer;
+
+var databaseUri = 'databaseUri'
+
+if (!databaseUri) {
+  console.log('DATABASE_URI not specified, falling back to localhost.');
+}
+
+var api = new ParseServer({
+  databaseURI: databaseUri || 'mongodb://localhost:27017/dev',
+  cloud: process.env.CLOUD_CODE_MAIN || __dirname + '/cloud/main.js',
+  appId: 'appId',
+  masterKey: 'masterKey'
+});
+
+var app = express();
+
+// Serve the Parse API on the /parse URL prefix
+var mountPath = process.env.PARSE_MOUNT || '/parse';
+app.use(mountPath, api);
+
+// Parse Server plays nicely with the rest of your web routes
+app.get('/', function(req, res) {
+  res.status(200).send('I dream of being a web site.');
+});
+
+var port = process.env.PORT || 1337;
+app.listen(port, function() {
+    console.log('parse-server-example running on port ' + port + '.');
+});
